649b5df6ea28ee3a20ccc835e015e1820bacb583
danieliko2 <69003248+danieliko2@users.noreply.github.com>
2021-12-14 23:14:48 +0200

    Add files via upload

---
+++ myCharts.js
@@ -0,0 +1,77 @@
+const dataBar = {
+    labels: ['1', '2', '3', '4', '5', '6'],
+    datasets: [{
+        fill: true,
+        label: 'החזר חודשי',
+        data: [1,2,3],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',],
+        //     'rgba(54, 162, 235, 0.2)',
+        //     'rgba(255, 206, 86, 0.2)',
+        //     'rgba(75, 192, 192, 0.2)',
+        //     'rgba(153, 102, 255, 0.2)',
+        //     'rgba(255, 159, 64, 0.2)'
+        // ],
+        // borderColor: [
+        //     'rgba(255, 99, 132, 1)',
+        //     'rgba(54, 162, 235, 1)',
+        //     'rgba(255, 206, 86, 1)',
+        //     'rgba(75, 192, 192, 1)',
+        //     'rgba(153, 102, 255, 1)',
+        //     'rgba(255, 159, 64, 1)'
+        // ],
+        borderWidth: 1
+    }]
+};
+const config = {
+type: 'line',
+data: dataBar,
+options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    }
+}
+}
+const myBarChart = new Chart(document.getElementById('myBarChart'), config);
+
+//const ctx2 = document.getElementById('myPieChart').getContext('2d');
+// const dataPie = {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [1,0,0,0,0,1],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     };
+// const configPie = {
+//     type: 'pie',
+//     data: dataPie,
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// };
+// const myPieChart = new Chart(document.getElementById('myPieChart').getContext('2d'), configPie);
---
+++ script1.js
@@ -0,0 +1,111 @@
+$("#inputAmount1").on("input", function () { // Amount changed
+    console.log("inpudchanged");
+    amount = parseInt(document.getElementById('inputAmount1').value);
+    percent = parseInt(document.getElementById('inputPercent1').value);
+    total = amount + (amount * (percent / 100));
+    document.getElementById('totalAmount1').innerHTML = "סכום כללי: " + total;
+    sessionStorage.setItem('totalAmount1', JSON.stringify(total));
+    let totalAmount = parseInt(sessionStorage.getItem('totalAmount1'));
+    console.log("total amount is " + totalAmount);
+});
+
+function valuesTest(){ 
+        console.log(sessionStorage.getItem("months"));
+}
+
+// let totalAmount = parseInt(sessionStorage.getItem('totalAmount1'));
+// let months = [];
+// let monthly=totalAmount/12;
+// months.push(monthly);
+// for(let i=1; i<6; i++){
+//     monthly=monthly*1.1;
+//     months.push(monthly)
+// }
+
+// sessionStorage.setItem("months", JSON.stringify(months));
+
+// function updateTable(){
+//     let maslulLength = $('.maslulim').length;
+//     for(let i = 1; i <= maslulLength; i++) {
+//         // console.log(`inputAmount${i}`);
+//         // console.log(document.getElementById(`inputAmount${i}`).value);
+//         months = [];
+//         amount = parseInt(document.getElementById(`inputAmount${i}`).value);    
+//         percent = parseInt(document.getElementById(`inputPercent${i}`).value);
+//         total = amount + (amount * (percent / 100));
+//         monthly=total/12;
+//         months.push(monthly);
+//         for(let i=1; i<6; i++){
+//             monthly=monthly*1.1;
+//             months.push(monthly)
+//         }
+//     }
+//     myBarChart.reset();
+//     console.log(months);
+//     myBarChart.configBar.data.datasets[0].data = months;
+//     myBarChart.update();
+// }
+
+function updateTable(){
+    let maslulLength = $('.maslulim').length;
+    for(let i = 1; i <= maslulLength; i++) {
+        months = [];
+        amount = parseInt(document.getElementById(`inputAmount${i}`).value);    
+        percent = parseInt(document.getElementById(`inputPercent${i}`).value);
+        total = amount + (amount * (percent / 100));
+        monthly=total/12;
+        months.push(monthly);
+        for(let i=1; i<6; i++){
+            monthly=monthly*1.1;
+            months.push(monthly)
+        }
+        console.log(myBarChart);
+        if (total>0){
+            set = {
+                fill: true,
+                label: 'החזר חודשי',
+                data: months,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)'],
+                //     'rgba(54, 162, 235, 0.2)',
+                //     'rgba(255, 206, 86, 0.2)',
+                //     'rgba(75, 192, 192, 0.2)',
+                //     'rgba(153, 102, 255, 0.2)',
+                //     'rgba(255, 159, 64, 0.2)'
+                // ],
+                // borderColor: [
+                //     'rgba(255, 99, 132, 1)',
+                //     'rgba(54, 162, 235, 1)',
+                //     'rgba(255, 206, 86, 1)',
+                //     'rgba(75, 192, 192, 1)',
+                //     'rgba(153, 102, 255, 1)',
+                //     'rgba(255, 159, 64, 1)'
+                // ],
+                borderWidth: 1
+            };
+            myBarChart.config.data.datasets[i-1] = set;
+            myBarChart.update();
+        }
+    }
+
+}
+
+function addMaslul(){
+    var maslulNum = $('.maslulim').length +1;
+    var div2=$(`<div class="maslulim" id="maslul${maslulNum}">
+                    <label for="inputPercent" style="font-size: 15px; color:gray;">אחוז</label>
+                    <input type="text" placeholder="אחוז" id="inputPercent${maslulNum}" data-type="number"/>
+                    <label for="inputAmount" style="font-size: 15px; color:gray;">סכום</label>
+                    <input type="text" placeholder="סכום" id="inputAmount${maslulNum}" data-type="number"/>
+                    <label for="inputAmount" style="font-size: 15px; color:gray;"> תקופה בחודשים</label>
+                    <input type="text" placeholder=" תקופה בחודשים" id="inputMonths${maslulNum}" data-type="number"/>
+                    <label for="inputAmount" style="font-size: 15px; color:gray;">ריבית</label>
+                    <input type="text" placeholder="ריבית" id="inputRibit${maslulNum}" data-type="number"/>
+                    <br>
+                    <a id="totalAmount${maslulNum}">סכום כללי: </a>
+                    <a id="monthlyAmount${maslulNum}">סכום חודשי: </a>
+                </div>`);
+    console.log(`#maslul${maslulNum-1}`);
+    var oldiv=$(`#maslul${maslulNum-1}`);
+    $(oldiv).after(div2);
+}
