3e2176eb5f1d9e3ed23ebc06487e7f3ce4348c7e
danieliko2 <danielkohav2@gmail.com>
2021-12-20 20:37:52 +0200

    New Tamhil and New Maslul working

--- script1.js
+++ script1.js
@@ -86,9 +86,6 @@ function updateTable(){
         }
     }
 }
-function newFunc(){
-    alert("newfunc");
-}
 function addMaslul(){
     var maslulNum = $('.maslulim').length +1;
     var div2=$(`<div class="maslulim" id="maslul${maslulNum}">
@@ -104,44 +101,41 @@ function addMaslul(){
                     <a id="totalAmount${maslulNum}">סכום כללי: </a>
                     <a id="monthlyAmount${maslulNum}">סכום חודשי: </a>
                 </div>`);
-    //console.log(`#maslul${maslulNum-1}`);
-    //var oldiv=$(`#maslul${maslulNum-1}`);
-    var oldiv = document.getElementById(`#maslul${maslulNum-1}`);
+    var oldiv = document.getElementById(`maslul${maslulNum-1}`);
     $(oldiv).after(div2);
     oldiv.style.visibility="none"
-    
 }
-function addTamhil(){
-    console.log("new tamhil clicked")
-    var ul = document.getElementById("tamhilimNavbar");
-    var liNum = $('ul#tamhilimNavbar li').length;
-    
-    console.log(liNum);
 
+function addTamhil(){
+    var ul = document.getElementById("tamhilimNavbar");  // create new navbar item
+    var liNum = $('ul#tamhilimNavbar li').length;
     document.getElementById("addTamhil").innerHTML=`תמהיל ${liNum}`;
     document.getElementById("addTamhil").setAttribute('id', `tamhil${liNum}NavItem`);
     document.getElementById(`tamhil${liNum}NavItem`).setAttribute('onClick', 'showTamhil(this.id)');
 
     var li = document.createElement("li");
-    //li.appendChild(document.createTextNode("הוסף תמהיל"));
     li.innerHTML=`<a class="navbar1Li" id="addTamhil" onclick="addTamhil()" href="javascript:void(null);">הוסף תמהיל</a>`;
     ul.appendChild(li);
 
     var tamhilN = $('.tamhilimNavbar').length +1;
     console.log(tamhilN);
     var tamhil2Li=$(`<a class="tamhilNavItem" id="tamhil${tamhilN}NavItem" onclick="showTamhil(this.id)" href="javascript:void(null);">tamhil${tamhilN}</a>`);
-    //console.log(div2);
     var oldTamhilLi=document.getElementById(`tamhil${tamhilN-1}NavItem`)
-    //$(oldTamhilLi).after(tamhil2Li);
 
-    //console.log(tamhilId);
-    var newDiv = document.createElement("div");
+
+    var newDiv = document.createElement("div"); // create new Tamhil
     newDiv.setAttribute(`id`, `tamhil${liNum}`);
     newDiv.innerHTML =`<input type="button" value="tamhil${liNum}">`;
     var oldDiv = document.getElementById(`tamhil${liNum-1}`);
-    //console.log(oldtamhil);
     $(oldDiv).after(newDiv);
-    oldDiv.style.display="none";
+    for (let i=1; i< liNum; i++){ // Set all tamhils to invisible
+        document.getElementById(`tamhil${i}NavItem`).style.backgroundColor = "grey";
+        document.getElementById(`tamhil${i}`).style.display = "none ";
+    }
+    document.getElementById(`tamhil${liNum}`).style.display = "block"; // active tamhil visible
+    document.getElementById(`tamhil${liNum}NavItem`).style.backgroundColor = "rgb(70, 69, 69)";
+
+
 }
 function showTamhil(tamhilId){
     var liNum = $('ul#tamhilimNavbar li').length;
@@ -152,7 +146,7 @@ function showTamhil(tamhilId){
     //let thisNum = tamhilId.slice(-8);
     //let thisNum = tamhilId[tamhilId.length-8];
     let thisNum = tamhilId.replace(/[^0-9]/g, ''); 
-    document.getElementById(`tamhil${thisNum}`).style.display = "block";
+    document.getElementById(`tamhil${thisNum}`).style.display = "flex";
     document.getElementById(`tamhil${thisNum}NavItem`).style.backgroundColor = "rgb(70, 69, 69)";
     //console.log(thisNum);
 }
